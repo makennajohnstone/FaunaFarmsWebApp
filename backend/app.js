7a5607e8970b119dc7e83278782640a5ba1d5334
Paul Studer <paulstuder@Pauls-MacBook-Pro.local>
2018-04-10 13:04:44 -0700

    db stuff

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,8 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var mysql = require("mysql");
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -24,6 +26,13 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+  res.locals.connection = mysql.createConnection({
+		host     : 'localhost',
+		user     : 'faunafarms',
+		password : 'ff',
+		database : 'test'
+	});
+	res.locals.connect();
   next(createError(404));
 });
 
